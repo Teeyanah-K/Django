243cb0ada320a59126af79d6d427f9adfdb6d406
Teeyanah-K <tianachris17@gmail.com>
2022-06-22 13:56:59 +0100

    more changes

---
+++ blog/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.urls import reverse
